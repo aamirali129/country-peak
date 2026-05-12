5a69fcbd525118c89c007c3eec4a1d45c165f270
aamirali129 <aamir.ali.s.129@kalvium.community>
2026-05-12 12:49:07 +0530

    Initial project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Home() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="page">
+      <h2>Home Page</h2>
+
+      <input
+        type="text"
+        placeholder="Search countries..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <p>Searching for: {search}</p>
+    </div>
+  );
+}
+
+export default Home;
